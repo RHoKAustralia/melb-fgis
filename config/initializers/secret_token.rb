7d6abb0adb8abb7edb459f5371e028aa6cd086be
Luke Chadwick <luke.a.chadwick@gmail.com>
2012-12-01 13:37:23 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fgis::Application.config.secret_token = 'c496bafce06dc4299cc1fd326d858bcfd5b6371108d49fe7fd836a6514985b4c9c99eda7e25711da0f4bea45ecc22134044262e5996d6c8cac2fa7242644a5d7'
