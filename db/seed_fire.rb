6ab18d19736da16682c8cfe1f90831a415327e20
Peter Donald <peter@realityforge.org>
2012-12-02 10:58:05 +1100

    Update the seed data

--- db/seed_fire.rb
+++ db/seed_fire.rb
@@ -1,1 +1,1 @@
-Fire.create!(:name => 'ABC123', :region => "POLYGON ((141.242425 -37.587984, 141.442425 -37.987984, 141.142425 -37.187984))")
+Fire.create!(:name => 'Boondoggle Rd, Burney Creek', :region => "POLYGON ((141.242425 -37.587984, 141.442425 -37.987984, 141.142425 -37.187984))")
